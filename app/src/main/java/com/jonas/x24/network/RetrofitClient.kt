e5e70013f3e20dec477128ccd2a509bb4749bcfe
Dtech2preas <lefa4082@gmail.com>
2026-01-27 20:19:10 +0200

    Update RetrofitClient.kt

--- app/src/main/java/com/jonas/x24/network/RetrofitClient.kt
+++ app/src/main/java/com/jonas/x24/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // NOTE: This URL should be updated if the worker is deployed to a different address.
-    private const val BASE_URL = "https://dark-mud-87fb.jonasmochebane.workers.dev/"
+    private const val BASE_URL = "https://super-surf-282e.jonasmochebane.workers.dev/"
 
     val api: WorkerApi by lazy {
         Retrofit.Builder()
