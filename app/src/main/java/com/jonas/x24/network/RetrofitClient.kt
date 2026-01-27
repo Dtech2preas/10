87d55ce941382d767418edea407b41213bf53521
Dtech2preas <lefa4082@gmail.com>
2026-01-27 20:59:31 +0200

    Update RetrofitClient.kt

--- app/src/main/java/com/jonas/x24/network/RetrofitClient.kt
+++ app/src/main/java/com/jonas/x24/network/RetrofitClient.kt
@@ -1,15 +1,24 @@
 package com.jonas.x24.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     // NOTE: This URL should be updated if the worker is deployed to a different address.
     private const val BASE_URL = "https://super-surf-282e.jonasmochebane.workers.dev/"
 
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(90, TimeUnit.SECONDS)
+        .readTimeout(90, TimeUnit.SECONDS)
+        .writeTimeout(90, TimeUnit.SECONDS)
+        .build()
+
     val api: WorkerApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(WorkerApi::class.java)
